ad6af2dee82555f87cac002f63a1983f66917750
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-02-04 18:23:56 +0100

    adds a verbose flag

--- src/Scrutinizer/Cli/OutputLogger.php
+++ src/Scrutinizer/Cli/OutputLogger.php
@@ -8,14 +8,20 @@ use Symfony\Component\Console\Output\OutputInterface;
 class OutputLogger extends AbstractLogger
 {
     private $output;
+    private $verbose;
 
-    public function __construct(OutputInterface $output)
+    public function __construct(OutputInterface $output, $verbose = false)
     {
         $this->output = $output;
+        $this->verbose = $verbose;
     }
 
     public function log($level, $message, array $context = array())
     {
+        if ( ! $this->verbose && $level === 'debug') {
+            return;
+        }
+
         $this->output->writeln($message);
     }
 }
