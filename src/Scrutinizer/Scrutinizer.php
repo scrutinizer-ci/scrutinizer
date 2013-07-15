552933f0d80d8a9b3e926e11f0a3fad3a3841e25
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-07-15 11:06:35 +0200

    adds some more logging commands

--- src/Scrutinizer/Scrutinizer.php
+++ src/Scrutinizer/Scrutinizer.php
@@ -78,11 +78,15 @@ class Scrutinizer
 
         $config = $this->getConfiguration()->process($rawConfig);
 
-        foreach ($config['before_commands'] as $cmd) {
-            $proc = new LoggableProcess($cmd, $dir);
-            $proc->setTimeout(300);
-            $proc->setLogger($this->logger);
-            $proc->run();
+        if ( ! empty($config['before_commands'])) {
+            $this->logger->info('Executing before commands');
+            foreach ($config['before_commands'] as $cmd) {
+                $this->logger->info(sprintf('Running "%s"...', $cmd));
+                $proc = new LoggableProcess($cmd, $dir);
+                $proc->setTimeout(300);
+                $proc->setLogger($this->logger);
+                $proc->run();
+            }
         }
 
         $project = new Project($dir, $config, $paths);
@@ -96,11 +100,15 @@ class Scrutinizer
             $analyzer->scrutinize($project);
         }
 
-        foreach ($config['after_commands'] as $cmd) {
-            $proc = new LoggableProcess($cmd, $dir);
-            $proc->setTimeout(300);
-            $proc->setLogger($this->logger);
-            $proc->run();
+        if ( ! empty($config['after_commands'])) {
+            $this->logger->info('Executing after commands');
+            foreach ($config['after_commands'] as $cmd) {
+                $this->logger->info(sprintf('Running "%s"...', $cmd));
+                $proc = new LoggableProcess($cmd, $dir);
+                $proc->setTimeout(300);
+                $proc->setLogger($this->logger);
+                $proc->run();
+            }
         }
 
         return $project;
