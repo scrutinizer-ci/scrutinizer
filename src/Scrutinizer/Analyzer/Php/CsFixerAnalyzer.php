dfe5b990fef8e0088d9ed06650be08c37ce20888
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-09-13 14:19:01 +0200

    improves configuration of PHPCS

--- src/Scrutinizer/Analyzer/Php/CsFixerAnalyzer.php
+++ src/Scrutinizer/Analyzer/Php/CsFixerAnalyzer.php
@@ -69,6 +69,7 @@ class CsFixerAnalyzer extends AbstractFileAnalyzer
                         ->defaultValue('psr1')
                     ->end()
                     ->arrayNode('fixers')
+                        ->addDefaultsIfNotSet()
                         ->attribute('type', 'choice')
                         ->attribute('depends_on', array('level' => 'custom'))
                         ->beforeNormalization()->always(function($v) {
@@ -81,48 +82,63 @@ class CsFixerAnalyzer extends AbstractFileAnalyzer
                         ->children()
                             ->booleanNode('indentation')
                                 ->attribute('label', 'Code must use 4 spaces for indenting, not tabs.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('linefeed')
                                 ->attribute('label', 'All PHP files must use the Unix LF (linefeed) line ending.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('trailing_spaces')
                                 ->attribute('label', 'Remove trailing whitespace at the end of lines.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('unused_use')
                                 ->attribute('label', 'Unused use statements must be removed.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('phpdoc_params')
                                 ->attribute('label', 'All items of the @param phpdoc tags must be aligned vertically.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('visibility')
                                 ->attribute('label', 'Visibility must be declared on all properties and methods; abstract and final must be declared before the visibility; static must be declared after the visibility.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('return')
                                 ->attribute('label', 'An empty line feed should precede a return statement.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('short_tag')
                                 ->attribute('label', 'PHP code must use the long <?php ?> tags or the short-echo <?= ?> tags; it must not use the other tag variations.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('braces')
                                 ->attribute('label', 'Opening braces for classes, interfaces, traits and methods must go on the next line, and closing braces must go on the next line after the body. Opening braces for control structures must go on the same line, and closing braces must go on the next line after the body.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('include')
                                 ->attribute('label', 'Include and file path should be divided with a single space. File path should not be placed under brackets.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('php_closing_tag')
                                 ->attribute('label', 'The closing ?> tag MUST be omitted from files containing only PHP.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('extra_empty_lines')
                                 ->attribute('label', 'Removes extra empty lines.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('controls_spaces')
                                 ->attribute('label', 'A single space should be between: the closing brace and the control, the control and the opening parenthese, the closing parenthese and the opening brace.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('elseif')
                                 ->attribute('label', 'The keyword elseif should be used instead of else if so that all control keywords looks like single words.')
+                                ->defaultFalse()
                             ->end()
                             ->booleanNode('eof_ending')
                                 ->attribute('label', 'A file must always end with an empty line feed.')
+                                ->defaultFalse()
                             ->end()
                         ->end()
                     ->end()
@@ -134,14 +150,7 @@ class CsFixerAnalyzer extends AbstractFileAnalyzer
     public function analyze(Project $project, File $file)
     {
         $command = $project->getGlobalConfig('command');
-        $fixers = $project->getFileConfig($file, 'fixers');
-
-        $options = '';
-        if ( ! empty($fixers)) {
-            $options .= '--fixers='.implode(',', $fixers);
-        } else {
-            $options .= '--level='.$project->getFileConfig($file, 'level');
-        }
+        $options = $this->getCommandOptions($project, $file);
 
         $fixedFile = $file->getOrCreateFixedFile();
         $tmpPath = $this->tmpDir.'/'.$file->getPath();
@@ -177,5 +186,20 @@ class CsFixerAnalyzer extends AbstractFileAnalyzer
 
         $fixedFile->setContent(file_get_contents($tmpPath));
         unlink($tmpPath);
+    }
+
+    private function getCommandOptions(Project $project, File $file)
+    {
+        $level = $project->getFileConfig($file, 'level');
+        if ($level === 'custom') {
+            $filters = array_filter($project->getFileConfig($file, 'fixers'));
+            if (empty($filters)) {
+                throw new \RuntimeException(sprintf('The fixing level was set to "custom", but not fixers were selected.'));
+            }
+
+            return '--fixers='.implode(',', $filters);
+        }
+
+        return '--level='.$level;
     }
 }
